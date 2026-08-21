a574fca738be23237a4c0b6afc4ab77b160951c0
sangeet456 <sangeet40404@gmail.com>
2026-08-21 12:42:47 -0400

    new code

--- leetcode2028.java
+++ leetcode2028.java
@@ -1,8 +1,28 @@
 //find missing observations//
 import java.util.*;
-class leetcode2028{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+class leetcode2028{class Solution {
+    public int[] missingRolls(int[] rolls, int mean, int n) {
+        int m = rolls.length;
+        int sum = mean*(n+m);
+        for(int i=0;i<m;i++){
+            sum -= rolls[i];
+        }
+        if(sum<n || sum >6*n){
+            return new int[0];
+        }
+        int avg = sum / n ;
+        int rem = sum%n;
+        int res[] = new int[n];
+        for(int i=0;i<n;i++){
+            res[i]=avg;
+            if(rem>0){
+                res[i]++;
+                rem --;
+            }
+        }
         
+        return res;
+    }
+}
     }
 }
