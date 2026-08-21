979f3e78f722e3b1398afd54a802b15300742cce
sangeet456 <sangeet40404@gmail.com>
2026-08-21 11:59:38 -0400

    new code

---
+++ leetcode2028.java
@@ -0,0 +1,8 @@
+//find missing observations//
+import java.util.*;
+class leetcode2028{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
