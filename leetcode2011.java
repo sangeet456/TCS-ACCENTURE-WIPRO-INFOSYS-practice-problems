42c9064ec47252a49002a49cf6e3af0dfd7cb205
sangeet456 <sangeet40404@gmail.com>
2026-08-21 13:31:30 -0400

    new code

---
+++ leetcode2011.java
@@ -0,0 +1,11 @@
+public class leetcode2011 {
+    class Solution {
+    public int finalValueAfterOperations(String[] op) {
+            int x =0;
+            for(int i=0;i<op.length;i++){
+                if(op[i].equals("--X") || op[i].equals("X--")) x--;
+                if(op[i].equals("++X") || op[i].equals("X++")) x++;
+            }
+   return x; }
+}
+}
