ddbb1024b9f6a7f30fe022d58a54d119a083843f
sangeet <sangeet@kali.sangeet>
2026-08-22 10:07:29 -0400

    new code

---
+++ leetcode1967.java
@@ -0,0 +1,10 @@
+public class leetcode1967 {
+    class Solution {
+    public int numOfStrings(String[] patterns, String word) {
+     int count =0;
+     for(int i =0;i<patterns.length;i++){
+        if(word.contains(patterns[i])) count ++;
+     }   
+    return count;}
+}
+}
